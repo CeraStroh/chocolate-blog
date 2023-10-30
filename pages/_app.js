5033aecd8fcd55c76f09cb8c0f17c08e5fb88ed6
CeraStroh <cera.stroh@outlook.com>
2023-10-30 18:14:48 -0500

    enable dark mode

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/global.css';
+import '../styles/variables.css'
+import ThemeToggle from '../components/theme.util.js'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <ThemeToggle />
+    <Component {...pageProps} />
+  </>
 }
